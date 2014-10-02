e5495db9f28b3fc1bd44c56c21e3aedb9a19e777
Alex Mathew <alexmathew003@gmail.com>
2014-10-03 04:10:22 +0530

    Add first set of exercises

---
+++ string.py
@@ -0,0 +1,60 @@
+# Basic string exercises
+# Fill in the definitions for the required functions. The main functions and the testing 
+# has been handled, so when you run a program, you will get an output of how many testcases
+# passed and how many didn't.
+
+
+# A. Odd Rotation
+def odd_rotation(number, word):
+	"""
+	The function takes a number and a word as its arguments. 
+	If the number is even, return the first three characters of the string.
+	If the number is odd, return a word formed by a concatenation of the last 2 letters 
+	and the first 2 letters of the word.
+	For example,
+	odd_rotation(2, 'apple') -> 'app'
+	odd_rotation(5, 'cabbage') -> 'geca'
+	"""
+	
+	# Add your code here
+	return 
+
+
+# B. Flip Case
+def flip_case(word):
+	"""
+	The function takes a word as its argument.
+	Return a word that has all lowercase characters swapped to uppercase and vice versa.
+	"""
+
+	# Add your code here
+	return
+
+
+# C. And/Or ?
+def and_or(word):
+	"""
+	The function takes a word as its argument.
+	If the word starts with 'a', return the index where the substring 'and' appears.
+	Otherwise, if the word ends with 'r', return the index where the substring 'or' appears. 
+	"""
+
+	# Add your code here
+	return
+
+
+# D. sin + cos
+def sincos(number):
+	"""
+	The function takes a number as its argument.
+	Return a string formed by taking the first 5 characters of the result of the expression - 
+		sin(number) + cos(number)
+
+	HINT : Look into the math module in more detail
+	"""
+
+	# Add your code here
+	return
+
+
+
