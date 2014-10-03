45613f64d82cb40ec843d19449e2f75db9740790
Alex Mathew <alexmathew003@gmail.com>
2014-10-03 22:51:01 +0530

    Add tests in programs, include input files

--- list.py
+++ list.py
@@ -10,7 +10,7 @@ def alphanum_score(words):
 	Returns a score generated as follows,
 		> Start with a net score of 0
 		> If the element is a alpha-string, +1
-		> If the element is a number, -1
+		> If the element is a number-string, -1
 	"""
 
 	# Add your code here
@@ -18,10 +18,10 @@ def alphanum_score(words):
 
 
 # B. Occurences
-def occurences(words, letter):
+def occurences(words):
 	"""
-	The function takes a list of words and a letter as its arguments.
-	Returns a list sorted by the number of times the given letter occurs.
+	The function takes a list of words as its argument.
+	Returns a list sorted by the number of times the letter 'a' occurs.
 	"""
 
 	# Add your code here
@@ -29,14 +29,42 @@ def occurences(words, letter):
 
 
 # C. Tuple Merge
-def function(tuples1, tuples2):
+def tuple_merge(tuples):
 	"""
-	The functions takes two lists of 2-tuples as its arguments.
+	The functions takes a list of 2-tuples as its argument.
 	Returns a list of the form - 
-		[tuples1 sorted by first element, tuples2 sorted by second element]
+		[tuples sorted by first element, tuples sorted by second element]
 	"""
 
 	# Add your code here
 	return
 
 
+def test(function, input, expected):
+	print '\nCASE :', input 
+	try:
+		assert function(input) == expected
+		print 'TEST CASE PASSED ! EXPECTED : %s \nOBTAINED : %s' % (expected, function(input))
+	except AssertionError:
+		print 'TEST CASE FAILED ! EXPECTED : %s \nOBTAINED : %s' % (expected, function(input))
+
+
+def main():
+	print '\nA. alphanum Score'
+	test(alphanum_score, ['abc', '11', 'cde', 'rt', '2', 'word'], 2)
+	test(alphanum_score, ['1', '2', 'a', '3', 'b'], -1)
+	test(alphanum_score, [], 0)
+
+	print '\nB. Occurences'
+	test(occurences, ['apple', 'aardvark', 'empty'], ['empty', 'apple', 'aardvark'])
+	test(occurences, ['pple', 'rdvrk', 'empty'], ['pple', 'rdvrk', 'empty'])
+
+	print '\nC. Tuple Merge'
+	test(tuple_merge, [(1, 2), (3, 1), (2, 0), (2, 1)], [(1, 2), (2, 0), (2, 1), (3, 1), (2, 0), (3, 1), (2, 1), (1, 2)])
+	test(tuple_merge, [(7, 5), (5, 5), (7, 7), (3, 2), (1, 4)], [(1, 4), (3, 2), (5, 5), (7, 5), (7, 7), (3, 2), (1, 4), (7, 5), (5, 5), (7, 7)])
+	test(tuple_merge, [(14, 6), (3, 13), (8, 1), (15, 2), (5, 13)], [(3, 13), (5, 13), (8, 1), (14, 6), (15, 2), (8, 1), (15, 2), (14, 6), (3, 13), (5, 13)])
+
+
+if __name__ == '__main__':
+	main()
+	
--- string.py
+++ string.py
@@ -5,15 +5,12 @@
 
 
 # A. Odd Rotation
-def odd_rotation(number, word):
+def odd_rotation(word):
 	"""
-	The function takes a number and a word as its arguments. 
-	If the number is even, return the first three characters of the string.
-	If the number is odd, return a word formed by a concatenation of the last 2 letters 
+	The function takes a word as its argument. 
+	If the length of the word is even, return the first three characters of the string.
+	If the length of the word is odd, return a word formed by a concatenation of the last 2 letters 
 	and the first 2 letters of the word.
-	For example,
-	odd_rotation(2, 'apple') -> 'app'
-	odd_rotation(5, 'cabbage') -> 'geca'
 	"""
 	
 	# Add your code here
@@ -57,4 +54,35 @@ def sincos(number):
 	return
 
 
+def test(function, input, expected):
+	print '\nCASE :', input 
+	try:
+		assert function(input) == expected
+		print 'TEST CASE PASSED ! EXPECTED : %s \nOBTAINED : %s' % (expected, function(input))
+	except AssertionError:
+		print 'TEST CASE FAILED ! EXPECTED : %s \nOBTAINED : %s' % (expected, function(input))
 
+
+def main():
+	print '\nA. Odd Rotation'
+	test(odd_rotation, "apple", "app")
+	test(odd_rotation, "orange", "geor")
+	test(odd_rotation, "watermelon", "onwa")
+
+	print '\nB. Flip Case'
+	test(flip_case, "abcDEfgH", "ABCdeFGh")
+	test(flip_case, "xyz", "XYZ")
+	test(flip_case, "abcD EFgh IJ", "ABCd efGH ij")
+
+	print '\nC. And/Or ?'
+	test(and_or, "ampersand", 6)
+	test(and_or, "mortar", 1)
+
+	print '\nD. sin + cos'
+	test(sincos, 45, '1.376')
+	test(sincos, 23, '-1.37')
+	test(sincos, 81, '0.146')
+
+
+if __name__ == '__main__':
+	main()
