1fe0d630c6f092c3746cc4eb64c87968783c5bc2
Alex Mathew <alexmathew003@gmail.com>
2014-10-03 10:10:57 +0530

    Add long exercise

---
+++ words.py
@@ -0,0 +1,23 @@
+# The Grand Exercise
+# This exercise covers most elements covered in the first three sessions. Time to put it all together !
+# You will be writing this program from scratch. Function definitions, boilerplate and everything. 
+# You don't necessarily have to finish the whole thing in the time allotted. Work on it as much as you
+# can. Take your time and make sure you can write this whole thing yourself. 
+# Refer to the documentation. Use StackOverflow. Ask one of us. And put it together yourself.
+
+# This is what you have to do.
+# > Run the program with a keyword argument, which can be --count or --set
+# 	i.e, python words.py --count (or) python words.py --set
+#   Handle the cases where the keyword argument is either not given, or not one of these two.
+# > Read in the contents of the file "input.txt"
+# > If the keyword argument is "--count", count the occurences of each word in the file text, 
+#   and store the counts in a dictionary. Now, generate a string built from the list of words, 
+#   sorted by occurence. Write this into "output.txt". So in your output file, you should have
+#   the most occurring word first, the second most occurring word next, and so on.
+# > If the keyword argument is "--set", generate a set of all words in the file text, and generate
+#   a string built from a sorted list of all the unique words in the text. Write this into "output.txt".
+#   So in your output file, you should have the alphabetically first word appearing first, and so on.
+
+# You might want to look into the join() method for strings. Check out help !
+
+# Code away !
