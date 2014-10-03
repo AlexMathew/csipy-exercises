9db5711ff0546c24b84201bda75b04afc35095bb
Alex Mathew <alexmathew003@gmail.com>
2014-10-03 10:06:40 +0530

    Add list exercises

---
+++ list.py
@@ -0,0 +1,42 @@
+# Basic list exercises
+# Fill in the definitions for the required functions. The main functions and the testing 
+# has been handled, so when you run a program, you will get an output of how many testcases
+# passed and how many didn't.
+
+# A. alphanum Score
+def alphanum_score(words):
+	"""
+	The function takes a list of words as its argument.
+	Returns a score generated as follows,
+		> Start with a net score of 0
+		> If the element is a alpha-string, +1
+		> If the element is a number, -1
+	"""
+
+	# Add your code here
+	return
+
+
+# B. Occurences
+def occurences(words, letter):
+	"""
+	The function takes a list of words and a letter as its arguments.
+	Returns a list sorted by the number of times the given letter occurs.
+	"""
+
+	# Add your code here
+	return
+
+
+# C. Tuple Merge
+def function(tuples1, tuples2):
+	"""
+	The functions takes two lists of 2-tuples as its arguments.
+	Returns a list of the form - 
+		[tuples1 sorted by first element, tuples2 sorted by second element]
+	"""
+
+	# Add your code here
+	return
+
+
